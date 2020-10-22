e6d693d59fcc82a94a5e9bb1224ecd5c18586612
Bilal <bilalatur2@gmail.com>
2020-10-22 18:03:09 -0300

    Register y login funcionales

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,19 +2,59 @@ const fs = require('fs');
 const path = require('path');
 const bcrypt=require('bcrypt');
 const {check, validationResult,body } = require('express-validator');
+let db = require('../database/models');
 
 let usersController = {
-    login: function(req,res){
-        res.render('users/login');
-    },
-    postLogin: function(req,res){
-        res.render('home')
-    },
     register: function(req,res){
         res.render('users/register');
     },
     postRegister: function(req,res){
-        res.render('users/login')
+        db.User.findOrCreate({
+            where:{
+                email: req.body.email
+            },
+            defaults:{
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                email: req.body.email,
+                calle: req.body.calle,
+                numero: req.body.numero,
+                localidad: req.body.localidad,
+                admin: false,
+                telefono: req.body.telefono,
+                contraseña: bcrypt.hashSync(req.body.contraseña, 10)
+            }
+        })
+        .then(function(usuario){
+            res.render('users/login')
+        })
+    },
+    login: function(req,res){
+        res.render('users/login');
+    },
+    postLogin: function(req,res){
+        db.User.findOne({
+            where:{
+                email: req.body.email
+            }
+        })
+        .then(function(usuarioALoguear){
+            if(usuarioALoguear != null){
+                if(usuarioALoguear.contraseña === bcrypt.compareSync(req.body.contraseña)){
+                    req.session.usuarioLogueado = usuarioALoguear;
+                    if(req.body.recordame != undefined){
+                        let expiracion = new Date(Date.now() + 900000);
+                        res.cookie('recordame', usuarioPorLoguearse.email, {expires: expiracion}); 
+                    }
+                    res.redirect('/')
+                } else {
+                    res.render('error')
+                }
+            } else {
+                res.render('error')
+            }
+        })
+        res.render('home')
     }
 }
 
---
+++ database/models/User.js
@@ -0,0 +1,48 @@
+const config = require("../config/config");
+
+module.exports = (sequelize, dataTypes) => {
+    let alias = "User";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre: {
+            type: dataTypes.STRING
+        },
+        apellido: {
+            type: dataTypes.STRING
+        },
+        calle: {
+            type: dataTypes.STRING
+        },
+        numero: {
+            type: dataTypes.INTEGER,
+        },
+        localidad: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        admin: {
+            type: dataTypes.BOOLEAN
+        },
+        telefono: {
+            type: dataTypes.DOUBLE
+        },
+        contraseña: {
+            type: dataTypes.STRING,
+        }
+    }
+    let config = {
+        tableName: "users", //el mismo nombre en el modelo
+        timestamps: false
+    }
+
+    const User = sequelize.define(alias,cols,config)
+
+
+    return User;
+}
